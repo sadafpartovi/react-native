562e79f460066db2c8f9f7af940a2f29a68246d0
Sadaf <sadaf.prt@gmail.com>
2021-10-14 09:59:17 -0700

    added Stack

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { blue } from '@material-ui/core/colors';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from './screens/Home';
+import ColorPalatte from './screens/ColorPalatte';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home}/>
+      <Stack.Screen name='ColorPalatte' component={ColorPalatte}/>
+
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
+
+
+
+export default App;
